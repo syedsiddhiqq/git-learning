f2563af9476bafa2d12d909805c6656e37c34182
Syed Ali <34442258+syedsiddhiqq@users.noreply.github.com>
2019-12-01 19:16:21 +0530

    Update TheSequence.cpp

--- TheSequence.cpp
+++ TheSequence.cpp
@@ -9,6 +9,7 @@ int main(){
        length = sequence.size();
        sequence[length-1] = to_string(i+1);
        sequence.append(sequence);
+        sequence.append(sequence);
     }
     cout<<sequence;
     return 0;
